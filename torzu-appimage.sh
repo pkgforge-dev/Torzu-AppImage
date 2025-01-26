9d9bf3ac9d7d166049bcea18ea299eac2a4a55fd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-01-26 12:57:34 -0400

    lower block size a bit

--- torzu-appimage.sh
+++ torzu-appimage.sh
@@ -110,7 +110,7 @@ echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S24 -B16 \
+	--compression zstd:level=22 -S23 -B16 \
 	--header uruntime \
 	-i ./AppDir -o Torzu-"$VERSION"-anylinux-"$ARCH".AppImage
 
