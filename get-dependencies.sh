9a94fdcf587445a518a01c0f4f82d21f83844f49
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-18 10:27:16 -0400

    build vasm in `get-dependencies.sh`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,54 +17,64 @@ LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/downloa
 OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
 
 pacman -Syu --noconfirm \
-  aom \
-  boost \
-  boost-libs \
-  catch2 \
-  clang \
-  cmake \
-  dav1d \
-  enet \
-  ffmpeg \
-  ffmpeg4.4 \
-  fmt \
-  gamemode \
-  gcc13 \
-  glslang \
-  glu \
-  haskell-gnutls \
-  hidapi \
-  libass \
-  libfdk-aac \
-  libopusenc \
-  libva \
-  libvpx \
-  libxi \
-  libxkbcommon-x11 \
-  libxss \
-  mbedtls2 \
-  meson \
-  nasm \
-  ninja \
-  nlohmann-json \
-  numactl \
-  qt5-base \
-  qt5ct \
-  qt5-multimedia \
-  qt5-wayland \
-  qt5-webengine \
-  sdl2 \
-  svt-av1 \
-  unzip \
-  vulkan-headers \
-  x264 \
-  x265 \
-  xcb-util-image \
-  xcb-util-renderutil \
-  xcb-util-wm \
-  xorg-server-xvfb \
-  zip \
-  zsync
+	aom \
+	base-devel \
+	boost \
+	boost-libs \
+	catch2 \
+	clang \
+	cmake \
+	curl \
+	dav1d \
+	enet \
+	ffmpeg \
+	ffmpeg4.4 \
+	fmt \
+	gamemode \
+	git \
+	gcc13 \
+	glslang \
+	glu \
+	haskell-gnutls \
+	hidapi \
+	libass \
+	libfdk-aac \
+	libopusenc \
+	libva \
+	libvpx \
+	libxi \
+	libxkbcommon-x11 \
+	libxss \
+	mbedtls2 \
+	mesa \
+	meson \
+	nasm \
+	ninja \
+	nlohmann-json \
+	numactl \
+	patchelf \
+	pipewire-audio \
+	pulseaudio \
+	pulseaudio-alsa \
+	qt5-base \
+	qt5ct \
+	qt5-multimedia \
+	qt5-wayland \
+	qt5-webengine \
+	sdl2 \
+	svt-av1 \
+	strace \
+	unzip \
+	vulkan-headers \
+	wget \
+	x264 \
+	x265 \
+	xcb-util-image \
+	xcb-util-renderutil \
+	xcb-util-wm \
+	xorg-server-xvfb \
+	zip \
+	zsync
 
 if [ "$(uname -m)" = 'x86_64' ]; then
 	pacman -Syu --noconfirm vulkan-intel haskell-gnutls gcc13 svt-av1
@@ -82,6 +92,24 @@ wget --retry-connrefused --tries=30 "$OPUS_URL" -O ./opus-nano.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
+
+
+echo "Making vasm dependency..."
+echo "---------------------------------------------------------------"
+
+sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+mkdir -p /usr/local/bin
+cp /usr/bin/makepkg /usr/local/bin
+
+sed -i 's|-O2|-O3|; s|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
+cat /etc/makepkg.conf
+
+git clone https://aur.archlinux.org/vasm vasm
+( cd vasm
+  makepkg -f
+  ls -la .
+  pacman --noconfirm -U *.pkg.tar.*
+)
 
 echo "All done!"
 echo "---------------------------------------------------------------"
