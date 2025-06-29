a31cb5232de98a8935614f4eb56e6f64b8372135
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-06-29 09:01:39 -0400

    build manually

--- torzu-appimage.sh
+++ torzu-appimage.sh
@@ -7,41 +7,50 @@ SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
 
-if [ "$ARCH" = 'x86_64' ]; then
-	echo "Making x86-64-v3 build of torzu"
+if [ "$1" = 'v3' ] && [ "$ARCH" = 'x86_64' ]; then
+	echo "Making x86-64-v3 optimized build of torzu..."
 	ARCH="${ARCH}_v3"
+	ARCH_FLAGS="-march=x86-64-v3 -O3 -flto=thin -DNDEBUG"
+elif [ "$ARCH" = 'x86_64' ]; then
+	echo "Making x86-64 generic build of torzu..."
+	ARCH_FLAGS="-march=x86-64 -mtune=generic -O3 -flto=thin -DNDEBUG"
+else
+	echo "Making aarch64 build of torzu..."
+	ARCH_FLAGS="-march=armv8-a -mtune=generic -O3 -flto=thin -DNDEBUG"
 fi
+
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
 # BUILD TORZU
-git clone https://aur.archlinux.org/torzu-git.git torzu && (
+git clone --recursive --depth 1 https://notabug.org/litucks/torzu.git ./torzu && (
 	cd ./torzu
-	if [ "$1" = 'v3' ]; then
-		sed -i 's/-march=[^"]*/-march=x86-64-v3/' ./PKGBUILD
-		sed -i 's/-march=x86-64 /-march=x86-64-v3 /' /etc/makepkg.conf # Do I need to do this as well?
-		cat /etc/makepkg.conf
-	else
-		sed -i 's/-march=[^"]*/-march=x86-64/' ./PKGBUILD
-	fi
-
-	# Fix weird bug with vulkan
-	sed -i 's/-DYUZU_USE_EXTERNAL_VULKAN_SPIRV_TOOLS=OFF/-DYUZU_USE_EXTERNAL_VULKAN_SPIRV_TOOLS=ON/' ./PKGBUILD
-
-	# Force translatiosn to be built
-	sed -i 's|TRANSLATION=OFF|TRANSLATION=ON|g' ./PKGBUILD
-
-	if ! grep -q -- '-O3' ./PKGBUILD; then
-		sed -i 's/-march=/-O3 -march=/' ./PKGBUILD
-	fi
-	
-	cat ./PKGBUILD
-	
-	makepkg -f
-	pacman --noconfirm -U ./*.pkg.tar.*
-	ls .
-	VERSION="$(awk -F'=' '/pkgver=/{print $2; exit}' ./PKGBUILD)"
+	mkdir build
+	cd build
+	cmake .. -GNinja \
+		-DYUZU_USE_BUNDLED_VCPKG=OFF               \
+		-DENABLE_QT6=ON                            \
+		-DYUZU_USE_BUNDLED_QT=OFF                  \
+		-DYUZU_USE_BUNDLED_FFMPEG=OFF              \
+		-DYUZU_TESTS=OFF                           \
+		-DYUZU_CMD=OFF                             \
+		-DYUZU_CHECK_SUBMODULES=OFF                \
+		-DYUZU_USE_LLVM_DEMANGLE=OFF               \
+		-DYUZU_USE_BUNDLED_SDL2=ON                 \
+		-DYUZU_USE_EXTERNAL_SDL2=OFF               \
+		-DYUZU_USE_EXTERNAL_VULKAN_SPIRV_TOOLS=ON  \
+		-DYUZU_ENABLE_LTO=ON                       \
+		-DCMAKE_POLICY_VERSION_MINIMUM=3.5         \
+		-DCMAKE_INSTALL_PREFIX=/usr                \
+		-DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error" \
+		-DCMAKE_C_FLAGS="$ARCH_FLAGS"              \
+		-DCMAKE_SYSTEM_PROCESSOR="$(uname -m)"     \
+		-DCMAKE_BUILD_TYPE=Release
+	ninja
+	sudo ninja install
+	VERSION="$(git rev-parse --short HEAD)"
 	echo "$VERSION" > ~/version
 )
+VERSION="$(cat ~/version)"
 
 # NOW MAKE APPIMAGE
 mkdir ./AppDir
@@ -55,28 +64,28 @@ cp -v /usr/share/icons/hicolor/scalable/apps/*torzu*.svg ./.DirIcon
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
 chmod +x ./sharun-aio
 xvfb-run -a ./sharun-aio l -p -v -e -s -k \
-	/usr/bin/yuzu                           \
-	/usr/lib/lib*GL*                        \
-	/usr/lib/dri/*                          \
-	/usr/lib/vdpau/*                        \
-	/usr/lib/libvulkan*                     \
-	/usr/lib/libXss.so*                     \
-	/usr/lib/libxcb-cursor.so*              \
-	/usr/lib/libXrandr.so*                  \
-	/usr/lib/libXi.so*                      \
-	/usr/lib/libdecor-0.so*                 \
-	/usr/lib/libgamemode.so*                \
-	/usr/lib/qt/plugins/audio/*             \
-	/usr/lib/qt/plugins/bearer/*            \
-	/usr/lib/qt/plugins/imageformats/*      \
-	/usr/lib/qt/plugins/iconengines/*       \
-	/usr/lib/qt/plugins/platform*/*         \
-	/usr/lib/qt/plugins/styles/*            \
-	/usr/lib/qt/plugins/xcbglintegrations/* \
-	/usr/lib/qt/plugins/wayland-*/*         \
-	/usr/lib/pulseaudio/*                   \
-	/usr/lib/pipewire-0.3/*                 \
-	/usr/lib/spa-0.2/*/*                    \
+	/usr/bin/yuzu                            \
+	/usr/lib/lib*GL*                         \
+	/usr/lib/dri/*                           \
+	/usr/lib/vdpau/*                         \
+	/usr/lib/libvulkan*                      \
+	/usr/lib/libXss.so*                      \
+	/usr/lib/libxcb-cursor.so*               \
+	/usr/lib/libXrandr.so*                   \
+	/usr/lib/libXi.so*                       \
+	/usr/lib/libdecor-0.so*                  \
+	/usr/lib/libgamemode.so*                 \
+	/usr/lib/qt6/plugins/audio/*             \
+	/usr/lib/qt6/plugins/bearer/*            \
+	/usr/lib/qt6/plugins/imageformats/*      \
+	/usr/lib/qt6/plugins/iconengines/*       \
+	/usr/lib/qt6/plugins/platform*/*         \
+	/usr/lib/qt6/plugins/styles/*            \
+	/usr/lib/qt6/plugins/xcbglintegrations/* \
+	/usr/lib/qt6/plugins/wayland-*/*         \
+	/usr/lib/pulseaudio/*                    \
+	/usr/lib/pipewire-0.3/*                  \
+	/usr/lib/spa-0.2/*/*                     \
 	/usr/lib/alsa-lib/*
 rm -f ./sharun-aio
 
