a0dd2d723b64fa3ec6821d007d09d53345538dd4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-06-29 11:14:30 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,14 +1,84 @@
 #!/bin/sh
 
 set -ex
+ARCH="$(uname -m)"
 
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+pacman -Syu --noconfirm \
+	aom                 \
+	base-devel          \
+	boost               \
+	boost-libs          \
+	catch2              \
+	clang               \
+	cmake               \
+	curl                \
+	dav1d               \
+	enet                \
+	ffmpeg              \
+	ffmpeg4.4           \
+	fmt                 \
+	gamemode            \
+	git                 \
+	gcc                 \
+	glslang             \
+	glu                 \
+	haskell-gnutls      \
+	hidapi              \
+	libass              \
+	libdecor            \
+	libfdk-aac          \
+	libopusenc          \
+	libva               \
+	libvpx              \
+	libxi               \
+	libxkbcommon-x11    \
+	libxss              \
+	mbedtls2            \
+	mesa                \
+	meson               \
+	nasm                \
+	ninja               \
+	nlohmann-json       \
+	numactl             \
+	pipewire-audio      \
+	pulseaudio          \
+	pulseaudio-alsa     \
+	qt6-base            \
+	qt6ct               \
+	qt6-multimedia      \
+	qt6-tools           \
+	qt6-wayland         \
+	sdl2                \
+	svt-av1             \
+	unzip               \
+	vulkan-headers      \
+	vulkan-nouveau      \
+	vulkan-radeon       \
+	wget                \
+	x264                \
+	x265                \
+	xcb-util-image      \
+	xcb-util-renderutil \
+	xcb-util-wm         \
+	xorg-server-xvfb    \
+	zip                 \
+	zsync
 
-if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
+
+case "$ARCH" in
+	'x86_64')  
+		PKG_TYPE='x86_64.pkg.tar.zst'
+		pacman -Syu --noconfirm vulkan-intel haskell-gnutls svt-av1
+		;;
+	'aarch64') 
+		PKG_TYPE='aarch64.pkg.tar.xz'
+		pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost
+		;;
+	''|*)      
+		echo "Unknown cpu arch: $ARCH" 
+		exit 1
+		;;
+esac
 
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
 FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
@@ -16,80 +86,13 @@ QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/c
 LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
 
-pacman -Syu --noconfirm \
-	aom \
-	base-devel \
-	boost \
-	boost-libs \
-	catch2 \
-	clang \
-	cmake \
-	curl \
-	dav1d \
-	enet \
-	ffmpeg \
-	ffmpeg4.4 \
-	fmt \
-	gamemode \
-	git \
-	gcc \
-	glslang \
-	glu \
-	haskell-gnutls \
-	hidapi \
-	libass \
-	libdecor \
-	libfdk-aac \
-	libopusenc \
-	libva \
-	libvpx \
-	libxi \
-	libxkbcommon-x11 \
-	libxss \
-	mbedtls2 \
-	mesa \
-	meson \
-	nasm \
-	ninja \
-	nlohmann-json \
-	numactl \
-	pipewire-audio \
-	pulseaudio \
-	pulseaudio-alsa \
-	qt6-base \
-	qt6ct \
-	qt6-multimedia \
-	qt6-tools \
-	qt6-wayland \
-	sdl2 \
-	svt-av1 \
-	unzip \
-	vulkan-headers \
-	vulkan-nouveau \
-	vulkan-radeon \
-	wget \
-	x264 \
-	x265 \
-	xcb-util-image \
-	xcb-util-renderutil \
-	xcb-util-wm \
-	xorg-server-xvfb \
-	zip \
-	zsync
-
-if [ "$(uname -m)" = 'x86_64' ]; then
-	pacman -Syu --noconfirm vulkan-intel haskell-gnutls svt-av1
-else
-	pacman -Syu --noconfirm vulkan-freedreno vulkan-panfrost
-fi
-
 echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LLVM_URL" -O ./llvm-libs.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$QT6_URL" -O ./qt6-base-iculess.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LLVM_URL"   -O ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$QT6_URL"    -O ./qt6-base-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2-iculess.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O ./ffmpeg-mini.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL" -O ./opus-nano.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"   -O ./opus-nano.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
