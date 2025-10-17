a50b368b237427fe40b6319adfbe20c07cf92258
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-17 01:42:14 -0400

    `SHARUN_ALLOW_SYS_VKICD` is now handled automatically

--- torzu-appimage.sh
+++ torzu-appimage.sh
@@ -66,11 +66,6 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/yuzu* /usr/lib/libgamemode.so*
 
-# allow using host vk for aarch64 given the sad situation
-if [ "$ARCH" = 'aarch64' ]; then 
-	echo 'SHARUN_ALLOW_SYS_VKICD=1' >> ./AppDir/.env
-fi
-
 # MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
